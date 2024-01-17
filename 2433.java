2522fd426fd2f3d1c13b77c7254ddbfb247d9f2f
YatinDora81 <yatin.dora81@gmail.com>
2024-01-17 10:45:38 +0530

    17-1-2024

---
+++ 2433.java
@@ -0,0 +1,9 @@
+// 2433. Find The Original Array of Prefix Xor
+class Solution {
+    public int[] findArray(int[] nums) {
+        int[] prefix=new int[nums.length];
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.length;i++) prefix[i]=nums[i]^nums[i-1];
+        return prefix;
+    }
+}
