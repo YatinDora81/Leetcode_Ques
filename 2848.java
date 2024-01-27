2142c54a712368bf204db0a6a753ffd29736a063
YatinDora81 <yatin.dora81@gmail.com>
2024-01-27 14:02:30 +0530

    27-1-2024

---
+++ 2848.java
@@ -0,0 +1,10 @@
+// 2848. Points That Intersect With Cars
+/*
+class Solution {
+    public int numberOfPoints(List<List<Integer>> nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.size();i++) for(int j=nums.get(i).get(0); j<= nums.get(i).get(1) ; j++) set.add(j);
+        return set.size();
+    }
+} 
+*/
