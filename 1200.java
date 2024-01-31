2fd9cfb2278c9146edb4b6402ab68d322f1c3bc2
YatinDora81 <yatin.dora81@gmail.com>
2024-01-31 11:48:39 +0530

    31-1-2024

---
+++ 1200.java
@@ -0,0 +1,27 @@
+// // 1200. Minimum Absolute Difference
+
+// class Solution {
+//     public List<List<Integer>> minimumAbsDifference(int[] arr) {
+//         List<List<Integer>> ans=new ArrayList<>();
+//         // for(int i=0;i<arr.length;i++){
+//         //     if(arr[i]<0) arr[i]=-arr[i];
+//         // }
+
+//         Arrays.sort(arr);
+//         int mini=Integer.MAX_VALUE;
+
+//         for(int i=1;i<arr.length;i++){
+//             mini=Math.min(mini,arr[i]-arr[i-1]);
+//         }
+
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]-arr[i-1] == mini) {
+//                 List<Integer> ds=new ArrayList<>();
+//                 ds.add(arr[i-1]);
+//                 ds.add(arr[i]);
+//                 ans.add(ds);
+//             }
+//         }
+//         return ans;
+//     }
+// }
