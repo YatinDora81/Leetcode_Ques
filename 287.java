fe057241b82dff32479df047a5745912083160e5
YatinDora81 <yatin.dora81@gmail.com>
2024-03-24 20:52:26 +0530

    24-March-2024

---
+++ 287.java
@@ -0,0 +1,9 @@
+// // 287. Find the Duplicate Number
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         HashSet<Integer> set=new HashSet<>();
+//         for(int i : nums){if(set.contains(i)) return i; set.add(i);}
+//         return 0;
+//     }
+// }
