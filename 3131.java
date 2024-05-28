5bcc3c1c77e72b06ed3e58fb4f79092bbdfdfa92
YatinDora81 <yatin.dora81@gmail.com>
2024-05-28 15:17:08 +0530

    28-May-2024

---
+++ 3131.java
@@ -0,0 +1,9 @@
+// // 3131. Find the Integer Added to Array I
+
+// class Solution {
+//     public int addedInteger(int[] nums1, int[] nums2) {
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
+//         return nums2[nums2.length-1] - nums1[nums1.length-1] ; 
+//     }
+// }
