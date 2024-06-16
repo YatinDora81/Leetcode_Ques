c01f007ecb0ca7114e2d64336b0545cf20d66932
YatinDora81 <yatin.dora81@gmail.com>
2024-06-16 15:51:12 +0530

    16-June-2024

--- 1963.java
+++ 1963.java
@@ -1,20 +1,20 @@
-// 1963. Minimum Number of Swaps to Make the String Balanced
+// // 1963. Minimum Number of Swaps to Make the String Balanced
 
 
-class Solution {
-    public int minSwaps(String s) {
-        int ans = 0;
-        int o = 0;
-        int c = 0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='[') o++;
-            else if( s.charAt(i)==']' && o>0 ) o--;
-            else c++;
-        }
+// class Solution {
+//     public int minSwaps(String s) {
+//         int ans = 0;
+//         int o = 0;
+//         int c = 0;
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i)=='[') o++;
+//             else if( s.charAt(i)==']' && o>0 ) o--;
+//             else c++;
+//         }
 
-        ans += o/2;
-        if(o%2!=0) ans++;
+//         ans += o/2;
+//         if(o%2!=0) ans++;
 
-        return ans;
-    }
-}
+//         return ans;
+//     }
+// }
