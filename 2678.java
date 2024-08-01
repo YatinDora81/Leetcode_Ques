db940b594591e25171199f67fabcec6caf0b06cb
YatinDora81 <yatin.dora81@gmail.com>
2024-08-01 11:04:23 +0530

    1-Aug-2024

---
+++ 2678.java
@@ -0,0 +1,9 @@
+// // 2678. Number of Senior Citizens
+
+// class Solution {
+//     public int countSeniors(String[] details) {
+//         int c=0;
+//         for(String s : details) if(Integer.parseInt(s.substring(11,13)) >60) c++;
+//         return c;
+//     }
+// }
