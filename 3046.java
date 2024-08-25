2608bde4a51656532ee3f26a4d11190bea6d7228
YatinDora81 <yatin.dora81@gmail.com>
2024-08-25 18:39:23 +0530

    24-Aug-2024

---
+++ 3046.java
@@ -0,0 +1,13 @@
+// // 3046. Split the Array
+
+// class Solution {
+//     public boolean isPossibleToSplit(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int i : nums){
+//             if(map.containsKey(i)) map.put(i,map.get(i)+1);
+//             else map.put(i,1);
+//             if(map.get(i)>2) return false;
+//         }
+//         return map.size() >= nums.length / 2;
+//     }
+// }
