81d89882c38aae8413732795c2586bff68611fee
YatinDora81 <yatin.dora81@gmail.com>
2024-09-09 19:38:59 +0530

    9-Sept-2024

---
+++ 3280.java
@@ -0,0 +1,7 @@
+// // 3280. Convert Date to Binary
+
+// class Solution {
+//     public String convertDateToBinary(String date) {
+//         return Integer.toBinaryString( Integer.parseInt(date.split("-")[0]) ) + "-" + Integer.toBinaryString( Integer.parseInt(date.split("-")[1]) ) + "-" + Integer.toBinaryString( Integer.parseInt(date.split("-")[2]) ) ;
+//     }
+// }
