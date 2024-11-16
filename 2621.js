3ad5a72cdcbb1f21d60cca6b22cd43dd18cdf39a
YatinDora81 <yatin.dora81@gmail.com>
2024-11-16 23:25:53 +0530

    16-Nov-2024

---
+++ 2621.js
@@ -0,0 +1,14 @@
+// // 2621. Sleep
+
+// /**
+//  * @param {number} millis
+//  * @return {Promise}
+//  */
+// async function sleep(millis) {
+//     return new Promise( ( res, rej )=>setTimeout( ()=>{res(1)} , millis ) )
+//  }
+ 
+//  /** 
+//   * let t = Date.now()
+//   * sleep(100).then(() => console.log(Date.now() - t)) // 100
+//   */
