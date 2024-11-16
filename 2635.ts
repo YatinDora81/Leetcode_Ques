dbfc2400866965a58d1e6282b944332eace8b47d
YatinDora81 <yatin.dora81@gmail.com>
2024-11-16 18:35:52 +0530

    16-Nov-2024

---
+++ 2635.ts
@@ -0,0 +1,9 @@
+// // 2635. Apply Transform Over Each Element in Array
+
+// function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+//     let objarr : number[] = []
+//     for(let i =0;i<arr.length ; i++){
+//         objarr.push( fn( arr[i] , i ) );
+//     }
+//     return objarr;   
+// };
