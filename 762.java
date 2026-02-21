73a38df0f09d08b2144fc6b811df2b9d5c2b97c0
yatindora81 <yatin.dora81@gmail.com>
2026-02-21 11:21:31 +0530

    Refactor countPrimeSetBits method to use binary string representation and caching for prime checks

--- 762.java
+++ 762.java
@@ -1,21 +1,52 @@
-// 762. Prime Number of Set Bits in Binary Representation
+// // // 762. Prime Number of Set Bits in Binary Representation
 
-class Solution {
-    public int countPrimeSetBits(int left, int right) {
-        int ans= 0;
-        for(int i=left;i<=right;i++){
-            if(isprime(Integer.bitCount(i))) ans++;
-        }
-        return ans;
+// // class Solution {
+// //     public int countPrimeSetBits(int left, int right) {
+// //         int ans= 0;
+// //         for(int i=left;i<=right;i++){
+// //             if(isprime(Integer.bitCount(i))) ans++;
+// //         }
+// //         return ans;
 
-    }
+// //     }
 
-    boolean isprime(int n){
-        if(n<2) return false;
-        if(n==2 || n==3) return true;
-        for(int i=2;i<=n/2;i++){
-            if(n%i==0) return false;
-        }
-        return true;
-    }
-}
+// //     boolean isprime(int n){
+// //         if(n<2) return false;
+// //         if(n==2 || n==3) return true;
+// //         for(int i=2;i<=n/2;i++){
+// //             if(n%i==0) return false;
+// //         }
+// //         return true;
+// //     }
+// // }
+
+
+
+
+// class Solution {
+//     public int countPrimeSetBits(int left, int right) {
+//         int ans = 0;
+//         HashMap<Integer,Boolean> map = new HashMap<>();
+//         for(int i = left;i<=right;i++){
+//            String s = Integer.toBinaryString(i);
+//            int c = 0;
+//            for(char ch : s.toCharArray()) if(ch=='1') c++;
+//            if(isPrime(c,map)) ans++;
+//         }
+//         return ans;
+//     }
+//     public boolean isPrime(int n,HashMap<Integer,Boolean> dp){
+//         if(n<=1) return false;
+//         if(n<=3) return true;
+//         if(dp.containsKey(n)) return dp.get(n);
+
+//         for(int i=2;i*i<=n;i++){
+//             if(n%i==0){
+//                 dp.put(n,false);
+//                 return false;
+//             }
+//         }
+//         dp.put(n,true);
+//         return true;
+//     }   
+// }
