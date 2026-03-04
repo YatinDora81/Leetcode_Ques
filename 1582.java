28f5f403b82b8daea7f68b68e223e8e552354c82
yatindora81 <yatin.dora81@gmail.com>
2026-03-04 11:32:26 +0530

    Refactor numSpecial method in 1582.java to improve readability and logic. Rename variable 'c' to 'ans' and update the is method to solve for special positions in a binary matrix.

--- 1582.java
+++ 1582.java
@@ -1,22 +1,45 @@
-// // 1582. Special Positions in a Binary Matrix
+// // // 1582. Special Positions in a Binary Matrix
+
+// // class Solution {
+// //     public int numSpecial(int[][] mat) {
+// //         int c = 0;
+// //         for(int i=0;i<mat.length;i++){
+// //             for(int j=0;j<mat[0].length;j++){
+// //                 if( mat[i][j]==1 && is(mat , i , j) ) c++;
+// //             }
+// //         }
+// //         return c;
+// //     }
+// //     public boolean is(int [][]mat , int r , int c){
+// //         for(int i=0;i<mat[0].length;i++){
+// //             if( mat[r][i]==1 && i!=c ) return false;
+// //         }
+// //         for(int i=0;i<mat.length;i++){
+// //             if( mat[i][c]==1 && i!=r ) return false;
+// //         }
+// //         return true;
+// //     }
+// // }
 
 // class Solution {
 //     public int numSpecial(int[][] mat) {
-//         int c = 0;
+//         int ans = 0;
 //         for(int i=0;i<mat.length;i++){
 //             for(int j=0;j<mat[0].length;j++){
-//                 if( mat[i][j]==1 && is(mat , i , j) ) c++;
+//                 if(mat[i][j]==1){
+//                     if(solve(mat,i,j)) ans++;
+//                 }
 //             }
 //         }
-//         return c;
+//         return ans;
 //     }
-//     public boolean is(int [][]mat , int r , int c){
-//         for(int i=0;i<mat[0].length;i++){
-//             if( mat[r][i]==1 && i!=c ) return false;
-//         }
-//         for(int i=0;i<mat.length;i++){
-//             if( mat[i][c]==1 && i!=r ) return false;
-//         }
+//     public boolean solve(int [][]mat,int r,int c){
+//         int cc = 0;
+//         for(int i=0;i<mat[0].length;i++) if(mat[r][i]==1) cc++;
+//         if(cc!=1) return false;
+//         cc = 0;
+//         for(int i=0;i<mat.length;i++) if(mat[i][c]==1) cc++;
+//         if(cc!=1) return false;
 //         return true;
 //     }
 // }
