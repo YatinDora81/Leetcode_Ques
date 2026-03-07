9429cc18ff9038f6ac44dae78550a4955a77381b
yatindora81 <yatin.dora81@gmail.com>
2026-03-07 23:01:39 +0530

    Refactor and enhance the implementation of the findCircleNum method in 547.java to improve clarity and functionality. Update variable names and ensure proper handling of the adjacency matrix for province counting using BFS.

--- 547.java
+++ 547.java
@@ -1,47 +1,72 @@
-// // 547. Number of Provinces
-// // bfs dfs both codes
+// // // 547. Number of Provinces
+// // // bfs dfs both codes
+
+// // class Solution {
+
+// //     public void dfs(int [][]graph,boolean vis[],int curr,int n){
+// //         vis[curr]=true;
+// //         for(int i=0;i<n;i++){
+// //             if(graph[curr][i]==1 && !vis[i]) dfs(graph,vis,i,n);
+// //         }
+// //     }
+
+// //     public int findCircleNum(int[][] graph) {
+// //         int n=graph.length;
+// //         boolean []vis=new boolean[n];
+// //         int ans=0;
+// //         // for(int i=0;i<n;i++){
+// //         //     if(!vis[i]){
+// //         //         ans++;
+// //         //         dfs(graph,vis,i,n);
+// //         //     }
+// //         // }
+
+// //         for(int i=0;i<n;i++){
+// //             if(!vis[i]){
+// //                 ans++;
+// //                 //bfs code
+// //                 Queue<Integer> q=new LinkedList<>();
+// //                 q.add(i);
+// //                 vis[i]=true;
+
+// //                 while(q.size()>0){
+// //                     int curr = q.remove();
+// //                     for(int j=0;j<n;j++){
+// //                         if(graph[curr][j]==1 && !vis[j] ){
+// //                             q.add(j);
+// //                             vis[curr]=true;
+// //                         }
+// //                     }
+// //                 }
+
+// //             }
+// //         }
+
+// //         return ans;
+// //     }
+// // }
 
 // class Solution {
-
-//     public void dfs(int [][]graph,boolean vis[],int curr,int n){
-//         vis[curr]=true;
-//         for(int i=0;i<n;i++){
-//             if(graph[curr][i]==1 && !vis[i]) dfs(graph,vis,i,n);
-//         }
-//     }
-
-//     public int findCircleNum(int[][] graph) {
-//         int n=graph.length;
-//         boolean []vis=new boolean[n];
-//         int ans=0;
-//         // for(int i=0;i<n;i++){
-//         //     if(!vis[i]){
-//         //         ans++;
-//         //         dfs(graph,vis,i,n);
-//         //     }
-//         // }
-
-//         for(int i=0;i<n;i++){
+//     public int findCircleNum(int[][] arr) {
+//         boolean vis[] = new boolean[arr.length];
+//         int ans = 0;
+//         for(int i=0;i<arr.length;i++){
 //             if(!vis[i]){
 //                 ans++;
-//                 //bfs code
-//                 Queue<Integer> q=new LinkedList<>();
+//                 Queue<Integer> q = new LinkedList<>();
 //                 q.add(i);
-//                 vis[i]=true;
-
+//                 vis[i] = true;
 //                 while(q.size()>0){
 //                     int curr = q.remove();
-//                     for(int j=0;j<n;j++){
-//                         if(graph[curr][j]==1 && !vis[j] ){
+//                     for(int j=0;j<arr[0].length;j++){
+//                         if(arr[curr][j]==1 && !vis[j]){
 //                             q.add(j);
-//                             vis[curr]=true;
+//                             vis[j] = true;
 //                         }
 //                     }
 //                 }
-
 //             }
 //         }
-
 //         return ans;
 //     }
 // }
