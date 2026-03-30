234e1975659c5aef5877fa202c73f1ce2ab55ead
yatindora81 <yatin.dora81@gmail.com>
2026-03-30 10:17:48 +0530

    Add initial implementation of checkStrings method in 2840.java to verify if two strings can be made equivalent by checking character counts at even and odd indices.

---
+++ 2840.java
@@ -0,0 +1,20 @@
+// class Solution {
+//     public boolean checkStrings(String s1, String s2) {
+//         HashMap<Character,HashMap<Boolean,Integer>> map = new HashMap<>();
+//         for(int i=0;i<s1.length();i++){
+//             char ch = s1.charAt(i);
+//             boolean is = i%2==0;
+//             if(!map.containsKey(ch)) map.put(ch,new HashMap<>());
+//             if(!map.get(ch).containsKey(is)) map.get(ch).put(is , 0);
+//             map.get(ch).put(is , map.get(ch).get(is) + 1);
+//         }
+        
+//         for(int i=0;i<s2.length();i++){
+//             char ch = s2.charAt(i);
+//             boolean is = i%2==0;
+//             if(!map.containsKey(ch) || !map.get(ch).containsKey(is) || map.get(ch).get(is)<=0) return false;
+//             map.get(ch).put(is , map.get(ch).get(is) - 1);
+//         }
+//         return true;
+//     }
+// }
